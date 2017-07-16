e7667a677b8ddca097a2e0ea76fb57385fccc973
karl-thomas <karl.thomas925@gmail.com>
2017-07-15 23:44:45 -0500

    refactors git adapter: removes token reader, DRYs up the options per each request.

--- app/models/github_adapter.rb
+++ app/models/github_adapter.rb
@@ -2,30 +2,30 @@ class GithubAdapter
   include HTTParty
   base_uri 'https://api.github.com'
 
-  attr_reader :user, :token
+  attr_reader :user
 
   def initialize(user, oauth_token)
     @user = user 
-    @token = oauth_token
+    @options = {Authorization: "token #{oauth_token}"}
   end
 
   def request_all_info
-    options = {Authorization: "token #{self.token}"}
-    self.class.get("/users/#{self.user}", options).parsed_response
+    self.class.get("/users/#{self.user}", @options).parsed_response
   end 
 
   def repos
-    options = {Authorization: "token #{self.token}"}
-    self.class.get("/users/#{self.user}/repos", options).parsed_response
+    self.class.get("/users/#{self.user}/repos", @options).parsed_response
+  end
+
+  def all_repo_names
+    self.repos.map {|repo| repo["name"]}
   end
 
   def all_commits
-    options = {Authorization: "token #{self.token}"}
-    self.class.get("/users/#{self.user}/repos", options).parsed_response
+    self.all_repo_names
   end
 
   def commits_for_repo(repo)
-    options = {Authorization: "token #{self.token}"}
-    self.class.get("/repos/#{self.user}/#{repo}", options).parsed_response
+    self.class.get("/repos/#{self.user}/#{repo}", @options).parsed_response
   end
 end
