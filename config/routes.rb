03ffa23f167f74ded2b0e3ed8a08cb855f688e6d
karl-thomas <karl.thomas925@gmail.com>
2017-07-15 23:09:33 -0500

    adds in the route to direct the repos action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'remote_api#github_profile'
+  root to: 'remote_api#github_profile'
   get 'remote_api/github_profile', to: 'remote_api#github_profile'
+  get 'remote_api/github_repos', to: 'remote_api#github_repos'
 end
