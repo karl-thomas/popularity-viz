40b612c24e69152ceb2f88af56b66165dba52456
karl-thomas <karl.thomas925@gmail.com>
2017-07-15 23:19:32 -0500

    refactors root path to redirect to instead of load github_profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'remote_api#github_profile'
+  
   get 'remote_api/github_profile', to: 'remote_api#github_profile'
   get 'remote_api/github_repos', to: 'remote_api#github_repos'
 end
