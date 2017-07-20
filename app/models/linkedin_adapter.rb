ef1a2480aa85edc67ee0e6811b30ea0cd4e1137d
karl-thomas <karl.thomas925@gmail.com>
2017-07-19 22:23:32 -0500

    through much messing about, linkedin now returns me.

--- app/models/linkedin_adapter.rb
+++ app/models/linkedin_adapter.rb
@@ -2,13 +2,18 @@ class LinkedinAdapter
   include HTTParty
   base_uri 'https://api.linkedin.com'
 
+  attr_reader :user
+
   def initialize
-    @auth = {"Authorization: Bearer #{ENV['LINKEDIN_ACCESS_TOKEN']}"}
+    @auth = {Authorization: ENV['LINKEDIN_ACCESS_TOKEN']}
     @options = {format: 'json'}
     @user = ENV['LINKEDIN_USER']
   end
 
   def profile
-    p self.class.get("/v1/profiles/#{self.user}", headers: @auth, query: @options)
+    p self.class.get("/v1/people/~?oauth2_access_token=#{@auth[:Authorization]}", query: @options)
   end
 end
+
+# == get all info, hopefully
+#https://api.linkedin.com/v1/people/~:(id,first-name,last-name,headline,picture-url,industry,summary,specialties,positions:(id,title,summary,start-date,end-date,is-current,company:(id,name,type,size,industry,ticker)),educations:(id,school-name,field-of-study,start-date,end-date,degree,activities,notes),associations,interests,num-recommenders,date-of-birth,publications:(id,title,publisher:(name),authors:(id,name),date,url,summary),patents:(id,title,summary,number,status:(id,name),office:(name),inventors:(id,name),date,url),languages:(id,language:(name),proficiency:(level,name)),skills:(id,skill:(name)),certifications:(id,name,authority:(name),number,start-date,end-date),courses:(id,name,number),recommendations-received:(id,recommendation-type,recommendation-text,recommender),honors-awards,three-current-positions,three-past-positions,volunteer)?oauth2_access_token=PUT_YOUR_TOKEN_HERE
