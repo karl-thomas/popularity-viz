a2bc979ebaf79fe8e8849a73bc6ea1f015de823a
karl-thomas <karl.thomas925@gmail.com>
2017-07-31 14:07:44 -0500

    finishes a srtring that pulls all basic profile info from linkedin

--- app/models/linkedin_adapter.rb
+++ app/models/linkedin_adapter.rb
@@ -11,9 +11,11 @@ class LinkedinAdapter
   end
 
   def profile
-    self.class.get("/v1/people/~?oauth2_access_token=#{@auth[:Authorization]}", query: @options)
+    profile_request = "/v1/people/~:(id,first-name,last-name,headline,picture-url,industry,num-connections,summary,current-share,specialties,positions:(id,title,summary,start-date,end-date,is-current,company:(id,name,type,size,industry,ticker)),educations:(id,school-name,field-of-study,start-date,end-date,degree,activities,notes),associations,interests,num-recommenders,date-of-birth,publications:(id,title,publisher:(name),authors:(id,name),date,url,summary),patents:(id,title,summary,number,status:(id,name),office:(name),inventors:(id,name),date,url),languages:(id,language:(name),proficiency:(level,name)),skills:(id,skill:(name)),certifications:(id,name,authority:(name),number,start-date,end-date),courses:(id,name,number),recommendations-received:(id,recommendation-type,recommendation-text,recommender),honors-awards,three-current-positions,three-past-positions,volunteer)" 
+       
+    self.class.get(profile_request + "?oauth2_access_token=#{@auth[:Authorization]}", query: @options)
   end
 end
 
 # == get all info, hopefully
-#https://api.linkedin.com/v1/people/~:(id,first-name,last-name,headline,picture-url,industry,summary,specialties,positions:(id,title,summary,start-date,end-date,is-current,company:(id,name,type,size,industry,ticker)),educations:(id,school-name,field-of-study,start-date,end-date,degree,activities,notes),associations,interests,num-recommenders,date-of-birth,publications:(id,title,publisher:(name),authors:(id,name),date,url,summary),patents:(id,title,summary,number,status:(id,name),office:(name),inventors:(id,name),date,url),languages:(id,language:(name),proficiency:(level,name)),skills:(id,skill:(name)),certifications:(id,name,authority:(name),number,start-date,end-date),courses:(id,name,number),recommendations-received:(id,recommendation-type,recommendation-text,recommender),honors-awards,three-current-positions,three-past-positions,volunteer)?oauth2_access_token=PUT_YOUR_TOKEN_HERE
+#https://api.linkedin.com/
