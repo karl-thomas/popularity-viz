9b5e7475d402fc23f0db2b4cc659747be2f0b093
karl-thomas <karl.thomas925@gmail.com>
2017-08-01 18:13:57 -0500

    TA#profile returns a json object of me

--- app/models/twitter_adapter.rb
+++ app/models/twitter_adapter.rb
@@ -1,4 +1,14 @@
 class TwitterAdapter
   include HTTParty
-  base_uri #??
+  base_uri "https://api.twitter.com"
+
+  def initialize
+    @user = ENV['TWITTER_USER']
+    @headers = {Authorization: "Bearer #{ENV['TWITTER_TOKEN']}"}
+  end
+
+  def profile
+    p @headers
+    p self.class.get("/1.1/users/lookup.json#{@user}", headers: @headers)
+  end
 end
