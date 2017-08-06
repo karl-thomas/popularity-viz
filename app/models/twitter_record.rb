d9fcc0f63d1b593850d2df6e6233d389845d1f1a
karl-thomas <karl.thomas925@gmail.com>
2017-08-05 21:23:17 -0500

    not much progress, but a breaking point

--- app/models/twitter_record.rb
+++ app/models/twitter_record.rb
@@ -1,9 +1,14 @@
 class TwitterRecord < ApplicationRecord
-  def initialize
+  def initialize(args={})
+    super(args)
     @twitter_adapter = TwitterAdapter.new
   end
 
   def compare_last_records_data
-    @old_record = TweetRecord.last
+    old_record = TweetRecord.last
+    old_data = old_record.attribute
+    # sue each pair to check old data against new data
+    
+    old_data.each_pair
   end
 end
