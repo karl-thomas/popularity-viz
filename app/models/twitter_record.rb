a33b0b99c2fc507e789ff25405184a92c9b6721f
karl-thomas <karl.thomas925@gmail.com>
2017-08-06 14:39:38 -0500

    make progress on the big comparative method, probably shoul refactor using self.

--- app/models/twitter_record.rb
+++ app/models/twitter_record.rb
@@ -6,9 +6,20 @@ class TwitterRecord < ApplicationRecord
 
   def compare_last_records_data
     old_record = TweetRecord.last
-    old_data = old_record.attribute
+    old_data = old_record.attributes
+    updated_data = @twitter_adapter.aggregate_user_data
     # sue each pair to check old data against new data
-    
-    old_data.each_pair
+
+    old_data.each_pair do |column_name, value|
+      case column_name
+      when "screen_name"
+        updated_data[column_name] if old_data[column_name] != updated_data[column_name.to_sym]
+        nil
+      when "description"
+        updated_data[column_name] if old_data[column_name] != updated_data[column_name.to_sym]
+        nil
+      when "followers_count"
+      end
+    end
   end
 end
