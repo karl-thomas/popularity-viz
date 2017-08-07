480e62200872c87278c8abc58e838f236099ad61
karl-thomas <karl.thomas925@gmail.com>
2017-08-07 00:19:24 -0500

    refactor case statement using comparitive statement, the comparitives set values to column that ar that are not there currently. but soon will be

--- app/models/twitter_record.rb
+++ app/models/twitter_record.rb
@@ -1,4 +1,5 @@
 class TwitterRecord < ApplicationRecord
+
   def initialize(args={})
     super(args)
     @twitter_adapter = TwitterAdapter.new
@@ -8,7 +9,6 @@ class TwitterRecord < ApplicationRecord
     old_record = TweetRecord.last
     old_data = old_record.attributes
     # sue each pair to check old data against new data
-
     old_data.map do |column_name, old_value|
       new_value = self.send(column_name)
 
@@ -17,45 +17,63 @@ class TwitterRecord < ApplicationRecord
         old_value != new_value ? new_value : nil
       when "description"
         old_value != new_value ? new_value : nil
-      when "followers_count"
-        if old_value != new_value
-          self.recent_followers = difference(old_value,new_value)
-        else
-          nil
-        end
-      when "friends_count"
-        if old_value != new_value
-          self.recent_friends = difference(old_value,new_value)
-        else
-          nil
-        end
-      when "tweets_count"
-        old_value
-      when "favorites_count"
-        if old_value != new_value
-          self.recent_favorites = difference(old_value,new_value)
-        else
-          nil
-        end
-      when "listed_count"
-        if old_value != new_value
-          self.recent_lists = difference(old_value,new_value)
-        else
-          nil
-        end
       when "current_status_id"
         old_value != new_value ? new_value : nil
+      when "followers_count"
+        compare_followers_count(old_value, new_value)
+      when "friends_count"
+        compare_friends_count(old_value, new_value)
+      when "favorites_count"
+        compare_favorites_count(old_value, new_value)
+      when "listed_count"
+        compare_lists_count(old_value, new_value)
+      when "tweets_count"
+        new_value
       when "recent_tweets"
-        old_value
+        new_value
       when "recent_mention"
-        old_value
+        new_value
       when "recent_replies"
-        old_value
+        new_value
+      else
+        nil
       end
     end
+  end
 
-    def difference(old_value, new_value)
-      return old_value - new_value
+  def compare_friends_count(old_value, new_value)
+    if old_value != new_value
+      self.recent_friends = difference(old_value,new_value)
+    else
+      nil
     end
+  end
+
+  def compare_followers_count(old_value, new_value)
+    if old_value != new_value
+      self.recent_followers = difference(old_value,new_value)
+    else
+      nil
+    end
+  end
+
+  def compare_favorites_count(old_value, new_value)
+    if old_value != new_value
+      self.recent_favorites = difference(old_value,new_value)
+    else
+      nil
+    end
+  end
+
+  def compare_lists_count(old_value, new_value)
+    if old_value != new_value
+      self.recent_lists = difference(old_value,new_value)
+    else
+      nil
+    end
+  end
+
+  def difference(old_value, new_value)
+    return new_value - old_value
   end
 end
