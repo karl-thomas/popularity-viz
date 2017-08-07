5a072e45daae5afc3d7d678e2086fa972f481dd0
karl-thomas <karl.thomas925@gmail.com>
2017-08-06 23:57:43 -0500

    complete the comparitive method, it need refactoring, it's so gross

--- app/models/twitter_record.rb
+++ app/models/twitter_record.rb
@@ -9,16 +9,53 @@ class TwitterRecord < ApplicationRecord
     old_data = old_record.attributes
     # sue each pair to check old data against new data
 
-    old_data.map do |column_name, value|
+    old_data.map do |column_name, old_value|
+      new_value = self.send(column_name)
+
       case column_name
       when "screen_name"
-        self.send(column_name) if value != self.send(column_name)
-        nil
+        old_value != new_value ? new_value : nil
       when "description"
-        self.send(column_name) if value != self.send(column_name)
-        nil
+        old_value != new_value ? new_value : nil
       when "followers_count"
+        if old_value != new_value
+          self.recent_followers = difference(old_value,new_value)
+        else
+          nil
+        end
+      when "friends_count"
+        if old_value != new_value
+          self.recent_friends = difference(old_value,new_value)
+        else
+          nil
+        end
+      when "tweets_count"
+        old_value
+      when "favorites_count"
+        if old_value != new_value
+          self.recent_favorites = difference(old_value,new_value)
+        else
+          nil
+        end
+      when "listed_count"
+        if old_value != new_value
+          self.recent_lists = difference(old_value,new_value)
+        else
+          nil
+        end
+      when "current_status_id"
+        old_value != new_value ? new_value : nil
+      when "recent_tweets"
+        old_value
+      when "recent_mention"
+        old_value
+      when "recent_replies"
+        old_value
       end
+    end
+
+    def difference(old_value, new_value)
+      return old_value - new_value
     end
   end
 end
