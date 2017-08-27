3df4ff9dcaac2fa33d750468ca8bcf91f0a22b26
karl-thomas <karl.thomas925@gmail.com>
2017-08-27 14:53:48 -0500

    tries to update rspotify to get oauth functioning

--- app/services/spotify_adapter.rb
+++ app/services/spotify_adapter.rb
@@ -1,7 +1,13 @@
 class SpotifyAdapter
+  attr_reader :user
+
   def initialize
     RSpotify.authenticate(ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"] )
     @user = ENV['SPOTIFY_USERNAME']
+  end
+
+  def profile 
+    RSpotify::User.find(self.user)
   end
 
 end
