953b00cf60d6d680951c3fee6cc40cf82670254c
karl-thomas <karl.thomas925@gmail.com>
2017-08-27 16:12:57 -0500

    writes out some methods to retrieve max info about all owned playlists

--- app/services/spotify_adapter.rb
+++ app/services/spotify_adapter.rb
@@ -7,7 +7,20 @@ class SpotifyAdapter
   end
 
   def profile 
-    RSpotify::User.find(self.user)
+    @profile = RSpotify::User.find(self.user)
   end
 
+  # for some reason you cant see track info from user.playlists, so you have to go find the playlists individually. 
+  def playlist_ids
+    self.profile.playlists.map { |playlist| playlist.id }
+  end
+
+  def full_playlist(id)
+    RSpotify::Playlist.find(self.user, id)
+  end
+
+  def all_playlists
+    array_of_ids = playlist_ids
+    array_of_ids.map { |id| self.full_playlist(id) }
+  end
 end
