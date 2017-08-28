45817a01611d0957a8da5f9aa66cd3562bdd0b28
karl-thomas <karl.thomas925@gmail.com>
2017-08-27 19:55:57 -0500

    accesses tracks and their audio fetures, currently working on a method to sum up the audio features

--- app/services/spotify_adapter.rb
+++ app/services/spotify_adapter.rb
@@ -1,6 +1,7 @@
 class SpotifyAdapter
   attr_reader :user, :two_weeks_ago
-
+  IMPORTANT_FEATURES = ["acousticness", "danceability", "duration_ms", "energy", "instrumentalness", "liveness", "loudness", "speechiness", "tempo", "valence"]
+  
   def initialize
     RSpotify.authenticate(ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"] )
     @user = ENV['SPOTIFY_USERNAME']
@@ -9,6 +10,14 @@ class SpotifyAdapter
 
   def profile 
     @profile ||= RSpotify::User.find(self.user)
+  end
+
+  def aggregate_data
+
+    {
+      recent_playlists: recent_playlists.count,
+      recently_added_tracks: recently_added_track_ids.count
+    }
   end
 
   def owned_playlists_short
@@ -33,11 +42,35 @@ class SpotifyAdapter
 
   def recent_playlists 
     playlists = self.owned_playlists_full
-    @recent_playlists ||= all_playlists.select { |playlist| recently_updated?(playlist) }
+    @recent_playlists ||= playlists.select { |playlist| recently_updated?(playlist) }
   end
 
   def recently_updated?(playlist)
     playlist.tracks_added_at.any? { |track, added_at| added_at > two_weeks_ago }
   end
-  
+
+  def recently_added_track_ids
+    recent_playlists.flat_map {|playlist| recent_tracks(playlist) }
+  end
+
+  def recent_tracks(playlist)
+    playlist.tracks_added_at.keys.select {|key| playlist.tracks_added_at[key] > two_weeks_ago}
+  end
+
+  def find_tracks(array_of_ids)
+    RSpotify::Track.find(array_of_ids)
+  end
+    
+
+  def average_audio_features
+    tracks = find_tracks(recently_added_track_ids)
+    tracks.reduce(Hash.new(0)) do |aggregate, track|
+      IMPORTANT_FEATURES.each do |feature|
+        value = track.audio_features.instance_variable_get("@#{feature}")
+        aggregate["average_#{feature}"] += value
+      end
+      aggregate
+    end
+  end
+
 end
