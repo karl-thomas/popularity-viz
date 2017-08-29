999c38c87494c04985cd1caf5193dc16469663e9
karl-thomas <karl.thomas925@gmail.com>
2017-08-28 19:53:47 -0500

    still trying to get oauth to work

--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -1,8 +1,17 @@
 class SpotifyController < ApplicationController
   before_action :set_adapter
 
+  def start
+
+  end
+
+  def index
+    redirect_to "https://accounts.spotify.com/authorize/?client_id=#{ENV['SPOTIFY_CLIENT_ID']}&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fspotify%2Fcallback&scope=user-read-private%20user-read-email%20user-library-read&state=rabbits"
+  end
+
  def callback
-  @spotify_user = RSpotify::User.new(request.env['rack.request.query_hash']['code'])
+  p "now im not"
+  @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
   binding.pry
  end
 
