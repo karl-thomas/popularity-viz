2228ea760ddbc886b3b5c838baec622c49227bfd
karl-thomas <karl.thomas925@gmail.com>
2017-08-29 22:27:29 -0500

    temporarily lock down spotify index route

--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -1,19 +1,12 @@
 class SpotifyController < ApplicationController
-  # before_action :set_adapter
-
   def index
+    redirect_to "http://google.com" if params[:rabbit] == ENV['RABBIT']
     @spotify_adapter = SpotifyAdapter.new
   end
 
- def spotify
-  p " DID I DO IT PLEASE TELL MEEEEEEEEE"
-  @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
-  binding.pry
- end
-
-  # private
-
-  #   def set_adapter
-  #     @spotify_adapter = SpotifyAdapter.new
-  #   end
+   def spotify
+    p " DID I DO IT PLEASE TELL MEEEEEEEEE"
+    @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+    binding.pry
+   end
 end
