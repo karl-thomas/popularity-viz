eb39232d8a34de3fd3798a07f04584f0b8a50d12
karl-thomas <karl.thomas925@gmail.com>
2017-08-29 22:23:51 -0500

    finally resolves oauth issues, user hash complete

--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -1,23 +1,19 @@
 class SpotifyController < ApplicationController
-  before_action :set_adapter
-
-  def start
-
-  end
+  # before_action :set_adapter
 
   def index
-    redirect_to "https://accounts.spotify.com/authorize/?client_id=#{ENV['SPOTIFY_CLIENT_ID']}&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fspotify%2Fcallback&scope=user-read-private%20user-read-email%20user-library-read&state=rabbits"
+    @spotify_adapter = SpotifyAdapter.new
   end
 
- def callback
-  p "now im not"
+ def spotify
+  p " DID I DO IT PLEASE TELL MEEEEEEEEE"
   @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
   binding.pry
  end
 
-  private
+  # private
 
-    def set_adapter
-      @spotify_adapter = SpotifyAdapter.new
-    end
+  #   def set_adapter
+  #     @spotify_adapter = SpotifyAdapter.new
+  #   end
 end
