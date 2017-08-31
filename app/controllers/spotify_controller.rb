431f0dc4e9ba9bacaaca0f29d503008d512ff069
karl-thomas <karl.thomas925@gmail.com>
2017-08-31 00:47:36 -0500

    changes oauth scope so i can access more fun info

--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -1,8 +1,8 @@
 class SpotifyController < ApplicationController
   def index
-  #   return redirect_to "http://google.com" if params[:rabbit] == "no"
-  #   @spotify_adapter = SpotifyAdapter.new
-  render body: "hey"
+    # return redirect_to "http://google.com" if params[:rabbit] == "no"
+    @spotify_adapter = SpotifyAdapter.new
+  # render body: "hey"
   end
 
    def spotify
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 require 'rspotify/oauth'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"], scope: 'user-read-email playlist-modify-public user-library-read user-library-modify'
+  provider :spotify, ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"], scope: 'user-read-email playlist-read-private user-library-read playlist-read-collaborative user-follow-read user-top-read user-read-currently-playing'
 end
