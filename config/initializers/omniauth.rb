8b80524b523460ed8228cb31fca8368aff85f25e
karl-thomas <karl.thomas925@gmail.com>
2017-09-01 00:54:34 -0500

    change the scope of the oauth tokens, again

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 require 'rspotify/oauth'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"], scope: 'user-read-email playlist-read-private user-library-read playlist-read-collaborative user-follow-read user-top-read user-read-currently-playing'
+  provider :spotify, ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"], scope: 'user-read-email playlist-read-private user-library-read playlist-read-collaborative user-follow-read user-top-read user-read-currently-playing user-read-recently-played'
 end
