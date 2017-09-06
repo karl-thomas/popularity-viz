d303b83ba55d884dcf78cc1f58e5bbe97de14981
karl-thomas <karl.thomas925@gmail.com>
2017-09-05 23:22:17 -0500

    updates spotify record model to count recent saved albums

--- app/models/spotify_record.rb
+++ app/models/spotify_record.rb
@@ -1,2 +1,9 @@
 class SpotifyRecord < ApplicationRecord
+  before_save :set_recent_saved_albums
+  def set_recent_saved_albums
+    old_record = SpotifyRecord.last
+    return nil if old_record.nil?
+    new_albums = self.saved_albums - old_record.saved_albums 
+    self.recent_saved_albums = new_albums
+  end
 end
