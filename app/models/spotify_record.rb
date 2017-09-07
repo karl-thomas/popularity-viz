8f44b3230b265741d5d4cef15c15bb8dc466b226
karl-thomas <karl.thomas925@gmail.com>
2017-09-06 23:40:11 -0500

    creates files for a github record

--- app/models/spotify_record.rb
+++ app/models/spotify_record.rb
@@ -1,7 +1,8 @@
 class SpotifyRecord < ApplicationRecord
   before_save :set_recent_saved_albums
+ 
   def set_recent_saved_albums
-    old_record = SpotifyRecord.last
+    old_record = SpotifyRecord.last || nil
     return nil if old_record.nil?
     new_albums = self.saved_albums - old_record.saved_albums 
     self.recent_saved_albums = new_albums
