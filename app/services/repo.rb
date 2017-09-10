c5bad9ece04435ea37592b7613afd0abc5406be1
karl-thomas <karl.thomas925@gmail.com>
2017-09-10 15:37:49 -0500

    adds in a way to see recent pull requests. also removes the self keyword from readers and methods

--- app/services/repo.rb
+++ app/services/repo.rb
@@ -16,16 +16,17 @@ class Repo < GithubAdapter
   # -- todo -- Pull requestin,
   def recent_pull_requests
     personal_client
-    self.client.pull_requests(id, since: two_weeks_ago)
+    all_pulls = client.pull_requests(id, state: 'all', since: two_weeks_ago)
+    all_pulls.select { |pull| pull[:created_at] > two_weeks_ago }
   end
 
   def collaborators
     personal_client
-    self.client.collaborators(self.full_name).pluck(:login)
+    client.collaborators(full_name).pluck(:login)
   end
 
   def recent?
-    self.updated_at > two_weeks_ago || self.pushed_at > two_weeks_ago
+    updated_at > two_weeks_ago || pushed_at > two_weeks_ago
   end
 
   def recent_commit_time_ranges
@@ -47,12 +48,12 @@ class Repo < GithubAdapter
 
   def recent_commits
     application_client
-    self.client.commits_since(self.full_name, two_weeks_ago, author: self.user)
+    client.commits_since(full_name, two_weeks_ago, author: user)
   end
 
   def all_commit_comments
     application_client
-    self.client.list_commit_comments(self.full_name)
+    client.list_commit_comments(full_name)
   end
 
   def recent_commit_comments
@@ -64,7 +65,7 @@ class Repo < GithubAdapter
 
   def deployments
     application_client
-    self.client.deployments(self.full_name)
+    client.deployments(full_name)
   end
 
   def recent_deployments
@@ -75,12 +76,12 @@ class Repo < GithubAdapter
 
   def branches
     application_client
-    self.client.branches(self.full_name)
+    client.branches(full_name)
   end
 
   def languages
     application_client
-    self.client.languages(self.full_name)
+    client.languages(full_name)
   end
 
   def most_used_lang
@@ -90,44 +91,45 @@ class Repo < GithubAdapter
   def dependent_repo_data
     { 
       repo: self,
-      recent_commits: self.recent_commits.count,
-      recent_comments: self.recent_commit_comments.count,
-      recent_deployments: self.recent_deployments.count,
-      branches: self.branches.count,
-      most_used_lang: self.most_used_lang
+      recent_pull_requests: recent_pull_requests.count,
+      recent_commits: recent_commits.count,
+      recent_comments: recent_commit_comments.count,
+      recent_deployments: recent_deployments.count,
+      branches: branches.count,
+      most_used_lang: most_used_lang
     }
   end
 
   def stargazers
     application_client
-    self.client.stargazers(self.full_name, accept: 'application/vnd.github.v3.star+json', auto_traversal: true).pluck(:user).pluck(:login)
+    client.stargazers(full_name, accept: 'application/vnd.github.v3.star+json', auto_traversal: true).pluck(:user).pluck(:login)
   end
 
   def recent_stargazers
-    self.stargazers.select { |stargazer| stargazer[:starred_at] > two_weeks_ago}
+    stargazers.select { |stargazer| stargazer[:starred_at] > two_weeks_ago}
   end
 
   def recent_clones
     personal_client
     media_type = "application/vnd.github.spiderman-preview"
-    self.client.clones(self.full_name, per: "week", accept: media_type)
+    client.clones(full_name, per: "week", accept: media_type)
   end
 
   def recent_views
     personal_client
     media_type = "application/vnd.github.spiderman-preview"      
-    self.client.views(self.full_name, per: "week", accept: media_type)
+    client.views(full_name, per: "week", accept: media_type)
   end
 
   def traffic_data
-    views = self.recent_views
+    views = recent_views
     {
-      repo_id: self.id,
+      repo_id: id,
       recent_views: views[:count],
-      recent_clones: self.recent_clones[:count],
+      recent_clones: recent_clones[:count],
       unique_views: views[:uniques],
-      recent_stargazers: self.recent_stargazers.count,
-      watchers: self.watchers_count
+      recent_stargazers: recent_stargazers.count,
+      watchers: watchers_count
     }  
   end
 end
