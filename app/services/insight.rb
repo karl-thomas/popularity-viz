5d53cfca456db4736acee9ebeb3f5b1d239819d3
karl-thomas <karl.thomas925@gmail.com>
2017-09-10 17:57:24 -0500

    makes a seeds file to create a new post out of all aggregate data, also updates insights so that i now have important info from the actual insight

--- app/services/insight.rb
+++ app/services/insight.rb
@@ -8,16 +8,19 @@ class Insight
   end
 
   def focused_song
-    stuff = []
     updated_repos.flat_map do |repo| 
       repo.recent_commit_time_ranges.map do |range|
-        songs = spotify_adapter.get_songs_after(range.first)["items"]
-        if song = songs.find {|song| song['played_at'] < range.last}
-          stuff << song
+        songs = spotify_adapter.get_songs_after(range.last)["items"]
+        if song = songs.find {|song| song['played_at'] < range.first}
+          binding.pry
+          song_hash = { track: song['track']['name'], 
+                        artist: song['track']['artists'].first['name'],
+                        played_at: song['played_at']
+                      }
+          return song_hash
         end
       end
     end
-    stuff
   end
 
   def updated_repos
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+g = GithubAdapter.new
+s = SpotifyAdapter.new
+t = TwitterAdapter.new
+
+insight_hash = {github: g, spotify: s, twitter: t}
+i = Insight.new(insight_hash)
+
+g_record = g.aggregate_data_record
+s_record = s.aggregate_data_record
+t_record = t.aggregate_data_record
+total_insights = i.total_insights
+
+post_hash = {github_record: g_record, spotify_record: s_record, twitter_record: t_record, insights: total_insights}
+
+post = Post.new(post_hash)
+
+# just incase i need to do somehting in betwwen init and save
+
+post.save
