f4d3b5abe82829c139859de57cc62dc13b7a1a78
karl-thomas <karl.thomas925@gmail.com>
2017-09-11 03:23:45 -0500

    comments out controllers i dont need

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,32 +1,32 @@
-# require_relative '../services/github_adapter_profile' 
+# # require_relative '../services/github_adapter_profile' 
 
-class GithubController < ApplicationController
-  before_action :set_github_adapter
+# class GithubController < ApplicationController
+#   before_action :set_github_adapter
 
-  def test
-    @api_response = @github_adapter
-    render json: @api_response.to_json
-  end
+#   def test
+#     @api_response = @github_adapter
+#     render json: @api_response.to_json
+#   end
 
-  def profile
-    @api_response = @github_adapter.profile
-    render json: @api_response.to_json
-  end
+#   def profile
+#     @api_response = @github_adapter.profile
+#     render json: @api_response.to_json
+#   end
 
-  def recent_repos
-    @api_response = @github_adapter.recent_repos
-    render json: @api_response["items"].to_json
-  end
+#   def recent_repos
+#     @api_response = @github_adapter.recent_repos
+#     render json: @api_response["items"].to_json
+#   end
 
-  def recent_commits
-    @api_response = @github_adapter.recent_commits
-    render json: @api_response.parsed_response.to_json
-  end
+#   def recent_commits
+#     @api_response = @github_adapter.recent_commits
+#     render json: @api_response.parsed_response.to_json
+#   end
 
-  private
+#   private
 
-    def set_github_adapter
-      @github_adapter = GithubAdapter.new
-    end
+#     def set_github_adapter
+#       @github_adapter = GithubAdapter.new
+#     end
 
-end
+# end
