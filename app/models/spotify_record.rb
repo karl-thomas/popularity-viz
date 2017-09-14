3e76a2980c9c0b2f79f5c81953a906d08d7c685b
karl-thomas <karl.thomas925@gmail.com>
2017-09-14 11:01:53 -0500

    fixes a bug with twitter data when ther is no  previous post

--- app/models/spotify_record.rb
+++ app/models/spotify_record.rb
@@ -1,8 +1,8 @@
-class SpotifyRecord 
-  def set_recent_saved_albums
-    old_record = SpotifyRecord.last || nil
-    return nil if old_record.nil?
-    new_albums = self.saved_albums - old_record.saved_albums 
-    self.recent_saved_albums = new_albums
-  end
-end
+# class SpotifyRecord 
+#   def set_recent_saved_albums
+#     old_record = SpotifyRecord.last || nil
+#     return nil if old_record.nil?
+#     new_albums = self.saved_albums - old_record.saved_albums 
+#     self.recent_saved_albums = new_albums
+#   end
+# end
