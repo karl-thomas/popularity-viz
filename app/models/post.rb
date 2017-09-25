5c69e86e415a05dba6905e6232a8ec8a48d03c39
karl-thomas <karl.thomas925@gmail.com>
2017-09-24 20:44:53 -0500

    calculates all recent internet happenings

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,16 +5,31 @@ class Post
   field :spotify_record, type: Hash
   field :github_record, type: Hash
   field :twitter_record, type: Hash
+  field :total_interactions, type: Integer
   field :insights, type: Hash
 
   attr_accessor :differences
   
-  before_save :inspect_old_data, :assign_total_differences
+  before_create :inspect_old_data
+  # when add_total_interactions gets before_create it does not add them up properly, after create, it does....
+  after_create :add_total_interactions
   
   # this is to give javascript something easy to read and follow some linting rules
   def to_json
     json_post = self.as_json
     json_post['id'] = json_post['_id'].to_s 
     json_post
+  end
+
+  def add_total_interactions
+    github_keys = self.github_record.select { |k,v| k.to_s.include?('recent')}
+    github_keys[:most_recent_project] = 0  # this in a non-countable value
+    spotify_keys = self.spotify_record.select { |k,v| k.to_s.include?('recent')}
+    twitter_keys = self.twitter_record.select { |k,v| k.to_s.include?('recent')}
+
+    # merge them all together and add them. 
+    total = github_keys.merge(twitter_keys).merge(spotify_keys).values.reduce(:+)
+    self.total_interactions = total
+    self.save
   end
 end
