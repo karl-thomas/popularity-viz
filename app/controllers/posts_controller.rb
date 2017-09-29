5c700fdc67f0af6730aa8745ca5f94d225332006
karl-thomas <karl.thomas925@gmail.com>
2017-09-29 11:10:56 -0500

    insights is now included as a module with post, for added integration

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,6 @@ class PostsController < ApplicationController
 
   def show
     post = Post.find(params[:id])
-    render json: relevant_data
+    render json: post.to_json
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,6 @@ s = SpotifyAdapter.new
 logger.info "initializing twitter adapter"
 t = TwitterAdapter.new
 
-insight_hash = {github: g, spotify: s, twitter: t}
-
-logger.info "initializing insights"
-i = Insight.new(insight_hash)
-
 logger.info "retrieving api data from github"
 g_record = g.aggregate_data_record
 
@@ -22,10 +17,7 @@ s_record = s.aggregate_data_record
 logger.info "retrieving api data from twitter"
 t_record = t.aggregate_data_record
 
-logger.info "retrieving insights on api data"
-total_insights = i.total_insights
-
-post_hash = { github_record: g_record, spotify_record: s_record, twitter_record: t_record, insights: total_insights }
+post_hash = { github_record: g_record, spotify_record: s_record, twitter_record: t_record}
 logger.info "creating new post"
 post = Post.new(post_hash)
 
