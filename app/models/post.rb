b9aa4f277634feb0a75b4afcf1b207d7569e8127
karl-thomas <karl.thomas925@gmail.com>
2017-10-01 21:28:51 -0500

    implements a post card method to send smaller amounts of data, this is to limit the amount of things i send through when something hits my index route, the personal website is not currently using all of the data from the index route, jsut a small portion of it... it also removes some of the old javascript config files i missed

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,6 +15,19 @@ class Post
   
   before_create :inspect_old_data, :add_total_interactions
   after_create :set_title, :set_insights 
+
+  def self.cards
+    self.all.map(&:card)
+  end
+
+  def card
+    {
+      id: self.id.to_s,
+      title: self.title,
+      total_interactions: self.total_interactions,
+      created_at: self.created_at
+    }
+  end
   
   # this is to give javascript something easy to read and follow some linting rules
   def to_json
