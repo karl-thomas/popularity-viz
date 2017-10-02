86d300716ef3ee6fdca93248014fef9ee737636a
karl-thomas <karl.thomas925@gmail.com>
2017-10-01 22:27:02 -0500

    tests broken, but pulls out repocollection methods into their own class, massively simplifying the github adapter class into more of a controller.

--- app/services/github_adapter.rb
+++ app/services/github_adapter.rb
@@ -36,8 +36,12 @@ class GithubAdapter
   end 
 
   def aggregate_data_record
-   profile_and_repos =  profile_data.merge(reduced_repo_data)
-   aggregate_record = profile_and_repos.merge(reduced_traffic_data)
+    # collect repo data.
+    dependent_repo_data = self.collaborated_repos.reduced_repo_data
+    traffic_data = self.owned_repos.reduced_traffic_data
+
+    profile_and_repos =  profile_data.merge(dependent_repo_data)
+    aggregate_record = profile_and_repos.merge(reduced_traffic_data)
   end
 
   def profile_data
@@ -70,27 +74,27 @@ class GithubAdapter
     return @owned_repos if @owned_repos
     application_client
     api_response = client.repos( user, affiliation: "owner" )
-    @owned_repos = convert_to_repos(api_response)
+    @owned_repos = RepoCollection.new(convert_to_repos(api_response))
   end
 
   def collaborated_repos
     return @collaborated_repos if @collaborated_repos
     application_client
     api_response = client.repos( user, affiliation: "collaborator" )
-    @collabor = convert_to_repos(api_response)
+    @collabor = RepoCollection.new(convert_to_repos(api_response))
   end
 
   def organizations_repos
     return @organizations_repos if @organizations_repos
     application_client
     api_response = client.repos( user, affiliation: "organization_member" )
-    @organizations_repos = convert_to_repos(api_response)
+    @organizations_repos = RepoCollection.new(convert_to_repos(api_response))
   end
 
   def starred_repos
     application_client
     api_response = client.starred(user)
-    @starred_repos ||= convert_to_repos(api_response)
+    @starred_repos ||= RepoCollection.new(convert_to_repos(api_response))
   end
 
   def convert_to_repos(sawyer_resources)
@@ -101,10 +105,6 @@ class GithubAdapter
     personal_client
     api_response = client.repository(id)
     Repo.new(api_response)
-  end
-
-  def recent_updated_repos(repos)
-    repos.select { |repo| repo.recent? }
   end
 
 
@@ -118,105 +118,5 @@ class GithubAdapter
     personal_client
     client.starred_gists( since: two_weeks_ago )
   end
-  
-  # recent project data --------------------------
-  # this data is for only recently updated repos
-  def collect_repo_data
-    recent_repos = recent_updated_repos(collaborated_repos)
-    # @var ||= for repeat method calls
-    @recent_repo_data ||= recent_repos.map {|repo| repo.dependent_repo_data}
-  end
-
-  def choose_most_used_language(recent_repo_data)
-    recent_repo_data.pluck(:most_used_lang).max_by do |lang, bytes|
-      bytes
-    end
-  end
-
-  def reduced_repo_data
-    repositories = collect_repo_data
-    language = choose_most_used_language(repositories)
-
-    reduced_data = repositories.reduce(Hash.new(0)) do |aggregate, pairs|   
-      pairs.each do |key, value|
-        choose_recent_project(repositories, aggregate, pairs)
-        reduce_repo_keys(aggregate, key, value)
-      end
-      aggregate
-    end
-    # add the most used language to the data. 
-    reduced_data.tap { |data| data[:most_used_lang] = language }
-  end
-
- 
-  # repo traffic, for all owned repos --------------
-  # this data is for all owned repos by the authorized user.
-  def collect_traffic_data
-    traffic_data = self.owned_repos.map {|repo| repo.traffic_data.to_h }
-  end
-
-  def most_viewed_repo
-    found_repo = self.owned_repos.max_by { |repo| repo.traffic_data.sum_of_interactions }
-    found_repo.traffic_data.to_h
-  end
-
-  def reduced_traffic_data
-    starting_data = collect_traffic_data
-
-    # melt all the traffic data of all repos into something digestable
-    reduced_data = starting_data.reduce(Hash.new(0)) do |aggregate, pairs|
-      pairs.each do |key, value|
-        reduce_uniques(aggregate, key, value)
-        reduce_traffic_keys(aggregate, key, value)
-      end
-      aggregate
-    end
-    # add the most viewed repo to the data
-    reduced_data.tap {|data| data[:most_viewed_repo] = most_viewed_repo}
-  end
-
-
-  private
-    def reduce_uniques(aggregate, key, views)
-      if key == :unique_views
-        aggregate[key] = 1 if aggregate[key] == 0 || aggregate[key] == nil
-        aggregate[key] += views - 1 if views > 1
-        aggregate
-      end
-    end
-
-    def reduce_traffic_keys(aggregate, key, value) 
-      if simple_traffic_reducers.include?(key)
-        aggregate[key] += value
-      end
-    end
-
-    def simple_traffic_reducers
-      [:recent_clones, :recent_views, :recent_stargazers, :watchers]
-    end
-
-    def reduce_repo_keys(aggregate, key, value) 
-      if simple_repo_reducers.include?(key)
-        aggregate[key] += value
-      end
-    end
-
-    def simple_repo_reducers
-      [:recent_comments, :recent_deployments, :recent_commits, :recent_pull_requests, :branches]
-    end
-
-    def choose_recent_project(collected_repositories, aggregate, pairs)
-      tracked_repo = sift_repo_data(collected_repositories, aggregate[:most_recent_project])
-      new_repo = sift_repo_data(collected_repositories, pairs[:repo].id)
-      if aggregate[:most_recent_project] == 0 
-        aggregate[:most_recent_project] = pairs[:repo].id
-      elsif tracked_repo[:repo].pushed_at < new_repo[:repo].pushed_at
-        aggregate[:most_recent_project] = pairs[:repo].id
-      end
-    end
-
-    def sift_repo_data(collected_repositories, id)
-      collected_repositories.find {|repo| repo[:repo].id == id}
-    end
 
 end
---
+++ app/services/repo_collection.rb
@@ -0,0 +1,102 @@
+class RepoCollection < Array 
+  
+  SIMPLE_REPO_REDUCERS =[:recent_comments, :recent_deployments, :recent_commits, :recent_pull_requests, :branches]
+  SIMPLE_TRAFFIC_REDUCERS = [:recent_clones, :recent_views, :recent_stargazers, :watchers]
+
+  attr_accessor :repos
+
+  def initialize(repos)
+    super
+    @repos = repos
+  end
+
+  def recent_repos
+    repos = self.select { |repo| repo.recent? }
+    RepoCollection.new(repos)
+  end 
+
+  def recent_repo_data
+    # @var ||= for repeat method calls
+    @recent_repo_data ||= recent_repos.map {|repo| repo.dependent_repo_data}
+  end
+
+  def most_used_language
+    recent_repo_data.pluck(:most_used_lang).max_by do |lang, bytes|
+      bytes
+    end
+  end
+
+  def reduced_repo_data
+    reduced_data = recent_repo_data.reduce(Hash.new(0)) do |aggregate, pairs|   
+      pairs.each do |key, value|
+        choose_recent_project(aggregate, pairs)
+        reduce_repo_keys(aggregate, key, value)
+      end
+      aggregate
+    end
+    # add the most used language to the data. 
+    @reduced_data = reduced_data.tap { |data| data[:most_used_lang] = most_used_language}
+  end
+
+  # traffic data. 
+  def collect_traffic_data
+    traffic_data = self.map {|repo| repo.traffic_data.to_h }
+  end
+
+  def most_viewed_repo
+    found_repo = self.max_by { |repo| repo.traffic_data.sum_of_interactions }
+    found_repo.traffic_data.to_h
+  end
+
+  def reduced_traffic_data
+    starting_data = collect_traffic_data
+
+    # melt all the traffic data of all repos into something digestable
+    reduced_data = starting_data.reduce(Hash.new(0)) do |aggregate, pairs|
+      pairs.each do |key, value|
+        reduce_uniques(aggregate, key, value)
+        reduce_traffic_keys(aggregate, key, value)
+      end
+      aggregate
+    end
+    # add the most viewed repo to the data
+    @reduced_traffic_data = reduced_data.tap {|data| data[:most_viewed_repo] = most_viewed_repo}
+  end
+
+  private 
+    # Unique views need to be handled in a different way than a normal key.
+    def reduce_uniques(aggregate, key, views)
+      if key == :unique_views
+        aggregate[key] = 1 if aggregate[key] == 0 || aggregate[key] == nil
+        aggregate[key] += views - 1 if views > 1
+        aggregate
+      end
+    end
+
+    def reduce_traffic_keys(aggregate, key, value) 
+      if SIMPLE_TRAFFIC_REDUCERS.include?(key)
+        aggregate[key] += value
+      end
+    end
+
+    def reduce_repo_keys(aggregate, key, value) 
+      if SIMPLE_REPO_REDUCERS.include?(key)
+        aggregate[key] += value
+      end
+    end
+
+    def choose_recent_project(aggregate, pairs)
+      tracked_repo = sift_repo_data(aggregate[:most_recent_project])
+      # p pairs[:repo].id
+      new_repo = sift_repo_data(pairs[:repo].id)
+      if aggregate[:most_recent_project] == 0 
+        aggregate[:most_recent_project] = pairs[:repo].id
+      elsif tracked_repo.pushed_at < new_repo.pushed_at
+        aggregate[:most_recent_project] = pairs[:repo].id
+      end
+    end
+
+    def sift_repo_data(id)
+      self.find {|repo| repo.id == id}
+    end
+end
