97255d4f5b3293f29fe6d333e8ed1ccd4639410d
karl-thomas <karl.thomas925@gmail.com>
2017-10-19 01:32:05 -0500

    add in pull requests class for ordering pull requests, fixes tests

--- spec/services/repo_commits_spec.rb
+++ spec/services/repo_commits_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Repo::CommitCollection do
+RSpec.describe Repo::Commits do
   let(:collection) { GithubAdapter.new.owned_repos.first.recent_commits}
   describe "on initialization", :vcr do
     describe "#commits" do
@@ -29,6 +29,22 @@ RSpec.describe Repo::CommitCollection do
 
     it "returns an array of String", :vcr do
       expect(collection.messages.first).to be_an_instance_of String
+    end
+  end
+
+  describe "group_per_day", :vcr do
+    it "returns a hash of dates of commits grouped", :vcr do
+      expect(collection.group_per_day).to be_an_instance_of Hash
+    end
+  end
+
+  describe "count_per_day", :vcr do
+    it "returns a hash of dates ", :vcr do
+      expect(collection.count_per_day).to be_an_instance_of Hash
+    end
+
+    it "has a value with a count of commits", :vcr do
+      expect(collection.count_per_day.values.first[:commits]).to be_an_instance_of Integer
     end
   end
 end
