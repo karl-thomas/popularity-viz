0077b3559976240a280cb7024b636d8d5c6d1eee
karl-thomas <karl.thomas925@gmail.com>
2017-11-01 19:12:13 -0500

    all tests green for SpotifyAdapter::Playlist

--- spec/services/spotify_adapter/spotify_adapter_playlist_spec.rb
+++ spec/services/spotify_adapter/spotify_adapter_playlist_spec.rb
@@ -25,15 +25,36 @@ RSpec.describe SpotifyAdapter::Playlist do
   end
 
   describe "#recent?", :vcr do
-
+    it "returns a boolean, if the playlist has been recently updated or not" do
+      expect(playlist.recent?).to be(true).or(false)
+    end
   end
 
   describe "#full", :vcr do
+    it "returns the full playlist from the spotify api" do
+      expect(playlist.full).to be_an_instance_of RSpotify::Playlist
+    end
+
+    it "needs to have #tracks_added_at" do
+      expect(playlist.full.tracks_added_at).not_to be nil
+    end
 
   end
 
   describe "#recent_tracks", :vcr do
+    it "returns a hash" do
+      expect(playlist.recent_tracks).to be_an_instance_of Hash
+    end
 
+    it "has keys of date objects, less than two weeks old" do
+      date = playlist.recent_tracks.keys.first
+      expect(date).to be > 2.weeks.ago
+    end
+
+    it "has strings of valid track ids for values" do
+      id =  playlist.recent_tracks.values.first
+      expect(id).to be_an_instance_of(String)
+    end
   end
 
 end
