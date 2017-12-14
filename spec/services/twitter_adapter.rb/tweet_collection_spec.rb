64ae2f8f79389fd89bd4979ad10cb9bfaa967d3d
karl-thomas <karl.thomas925@gmail.com>
2017-12-13 22:50:50 -0800

    set up skeleton tests for all twitter adapter behavior

---
+++ spec/services/twitter_adapter.rb/tweet_collection_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe TwitterAdapter::TweetCollection do 
+
+  describe "on initialization", :vcr  do
+
+  end
+
+  describe "#grouped_by_date", :vcr  do
+
+  end
+
+  describe "#count_by_date", :vcr  do
+
+  end
+end
---
+++ spec/services/twitter_adapter.rb/twitter_adapter_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe TwitterAdapter do 
+
+  describe "on initialization", :vcr do
+
+  end
+
+  describe "#retrieve_profile", :vcr do
+
+  end
+
+  describe "#aggregate_data_record", :vcr do
+
+  end
+
+  describe "#counts_by_date", :vcr do
+
+  end
+
+  describe "#recent_tweets", :vcr do
+
+  end
+
+  describe "#recent_replies", :vcr do
+
+  end
+
+  describe "#recent_mentions", :vcr do
+
+  end
+
+  describe "#retrieve_followers", :vcr do
+
+  end
+
+  describe "#retrieve_friends", :vcr do
+
+  end
+
+  describe "#retrieve_favorites", :vcr do
+
+  end
+
+  describe "#formatted_profile", :vcr do
+
+  end  
+end
