ac203a6a0f2b99b1abdd8f7f9bfa443efdba6a8b
karl-thomas <karl.thomas925@gmail.com>
2017-12-15 23:21:53 -0700

    start on the twitter collection tests

--- spec/services/twitter_adapter.rb/tweet_collection_spec.rb
+++ spec/services/twitter_adapter.rb/tweet_collection_spec.rb
@@ -1,16 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe TwitterAdapter::TweetCollection do 
+  let(:collection) { TwitterAdapter.new.recent_tweets}
 
   describe "on initialization", :vcr  do
+    describe "tweets" do
+      expect(collection.tweets).not_to be_nil
+    end 
 
+    describe "mode" do
+      expect(collection.mode).not_to be_nil
+    end 
   end
 
   describe "#grouped_by_date", :vcr  do
-
+    
   end
 
   describe "#count_by_date", :vcr  do
-
+    
   end
 end
