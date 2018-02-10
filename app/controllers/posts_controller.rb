548f1742e0b384a7c211e849fb4ee9063381a398
karl-thomas <karl.thomas925@gmail.com>
2018-02-09 22:49:19 -0800

    add basic auth to api requests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController 
-
+  before_action :authenticate 
+  
   def index
     render json: Post.cards
   end
@@ -8,4 +9,13 @@ class PostsController < ApplicationController
     post = Post.find(params[:id])
     render json: post.to_json
   end
+  
+  private
+    def authenticate
+      if request.method == "GET"
+        authenticate_or_request_with_http_basic do |username, password|
+          username == ENV["USERNAME"] && password == ENV["PASSWORD"]
+        end
+      end
+    end
 end
