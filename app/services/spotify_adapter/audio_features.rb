620272b81db7054a4b6eebca224933b74b12b1fc
karl-thomas <karl.thomas925@gmail.com>
2018-03-11 04:44:30 -0500

    pull out audio features into it's own section

--- app/services/spotify_adapter/audio_features.rb
+++ app/services/spotify_adapter/audio_features.rb
@@ -1,4 +1,10 @@
 class AudioFeatures
+  IMPORTANT_FEATURES = ["acousticness", "danceability", "duration_ms", "energy", "instrumentalness", "speechiness", "tempo", "valence"]
+
+  attr_reader :features
+  
+  # makes an api request to gather audio feeatures, 
+  # but they may not be there.
   def initialize(track)
     begin
       @features = track.audio_features
@@ -7,6 +13,12 @@ class AudioFeatures
     end
   end
 
+  # grab the most important features(useable) from the audio features, 
+  # it does not operate like a hash, so you must send a method to the RSpotifys wrapper. 
   def important_features
+    return nil if features.nil?
+    @important_features ||= self.class::IMPORTANT_FEATURES.map do |feature| 
+      [feature ,features.send(feature)] 
+    end.to_h
   end
 end
--- spec/services/spotify_adapter/spotify_adapter_track_audio_features_spec.rb
+++ spec/services/spotify_adapter/spotify_adapter_track_audio_features_spec.rb
@@ -1,8 +1,29 @@
 require "rails_helper"
 
-RSpec.describe SpotifyAdapter::Track::AudioFeatures :vcr do 
-  describe "initialization" :vcr do
-    context "when the track has audio features on spotify" :vcr do
+RSpec.describe SpotifyAdapter::Track::AudioFeatures, :vcr do 
+  let(:criminal_image_id) { "5Exvn8HMcR5siCQ4DdD0Sa"}
+  let(:time) {1.weeks.ago}
+  let(:track) { SpotifyAdapter::Track.new(criminal_image_id, time)}
+
+  describe "initialization", :vcr do
+    context "when the track has audio features on spotify", :vcr do
+      it "makes a request to the spotify api for audo featues" do
+        features = track.audio_features
+        assert_requested :get, "https://api.spotify.com/v1/audio-features/#{criminal_image_id}"
+      end
+
+      it "assigns data from this Rspotify Wrapper to features" do
+        features = track.audio_features
+        expect(features.features).to be_an_instance_of RSpotify::AudioFeatures
+      end
+    end
+  end
+
+  describe "important features" do
+    it "returns a hash of useful audio data" do
+      features = track.audio_features
+      expect(features.important_features).to be_an_instance_of Hash
     end
   end
 end
+
