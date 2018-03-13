6d771044495311af132dc2340365df9d60bbfbfc
karl-thomas <karl.thomas925@gmail.com>
2018-03-13 05:12:38 -0500

    convert playlist tests to expect array data instead of hashes

--- spec/services/twitter_adapter.rb/twitter_adapter_spec.rb
+++ spec/services/twitter_adapter.rb/twitter_adapter_spec.rb
@@ -2,37 +2,7 @@ require 'rails_helper'
 
 RSpec.describe TwitterAdapter do 
   let(:adapter) { TwitterAdapter.new }
-  describe "on initialization", :vcr do
-    describe "#user" do
-      it "exists" do
-        expect(adapter.user).not_to raise_error
-      end
 
-      it "is the env key for twitter users" do
-        expect(adapter.user).to eq twitter_user
-      end
-    end
-
-    describe "#client" do
-      it "exists" do
-        expect(adapter.client).not_to raise_error
-      end
-
-      it "returns an instance of Twitter::REST::Client" do
-        expect(adapter.client).to be_an_instance_of Twitter::REST::Client
-      end
-    end
-
-    describe "#two_weeks_ago" do
-      it "exists" do
-        expect(adapter.two_weeks_ago).not_to raise_error
-      end
-
-      it "returns a string of a date" do
-        expect(Date.parse(adapter.two_weeks_ago)).not_to raise_error
-      end
-    end
-  end
 
   describe "#retrieve_profile", :vcr do
     it "returns a Twitter::User object" do
@@ -41,11 +11,6 @@ RSpec.describe TwitterAdapter do
 
     it "should return the twitter user from the ENV" do
       expect(adapter.retrieve_profile.username).to eq twitter_user
-    end
-    
-    it "makes a request to the twitter api" do
-      adapter.retrieve_profile
-      assert_requested :get, ''
     end
 
   end
@@ -78,17 +43,10 @@ RSpec.describe TwitterAdapter do
     it "returns a tweet collection obj" do
       expect(collection).to be_an_instance_of TwitterAdapter::TweetCollection
     end
-
-    it "makes a request to the twitter api" do
-      assert_requested :get, ''
-    end
   end
 
   describe "#recent_replies", :vcr do
-    let(:collection) { adapter.recent_replies }
-    it "makes a request to the twitter api" do
-      assert_requested :get, ''
-    end
+    let(:collection) { adapter.recent_replies 
 
     it "returns a tweet collection obj" do
       expect(collection).to be_an_instance_of TwitterAdapter::TweetCollection
@@ -96,10 +54,7 @@ RSpec.describe TwitterAdapter do
   end
 
   describe "#recent_mentions", :vcr do
-    let(:collection) { adapter.recent_mentions }
-    it "makes a request to the twitter api" do
-      assert_requested :get, ''
-    end
+    let(:collection) { adapter.recent_mentions 
 
     it "returns a tweet collection obj" do
       expect(collection).to be_an_instance_of TwitterAdapter::TweetCollection
