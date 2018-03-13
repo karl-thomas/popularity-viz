b300c3f4d6346825a8b904cba6589b3ac3c00c4c
karl-thomas <karl.thomas925@gmail.com>
2018-03-13 04:48:53 -0500

    clean up twitter tests for rewrite, the twitter api seems to be working, but tweet collection needs looking at

--- spec/services/twitter_adapter.rb/tweet_collection_spec.rb
+++ spec/services/twitter_adapter.rb/tweet_collection_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe TwitterAdapter::TweetCollection do 
-  let(:collection) { TwitterAdapter.new.recent_tweets}
+  # let(:collection) { TwitterAdapter.new.recent_tweets}
 
-  describe "on initialization", :vcr  do
-    describe "tweets" do
-      expect(collection.tweets).not_to be_nil
-    end 
+  # describe "on initialization", :vcr  do
+  #   describe "tweets" do
+  #     expect(collection.tweets).not_to be_nil
+  #   end 
 
-    describe "mode" do
-      expect(collection.mode).not_to be_nil
-    end 
-  end
+  #   describe "mode" do
+  #     expect(collection.mode).not_to be_nil
+  #   end 
+  # end
 
-  describe "#grouped_by_date", :vcr  do
+  # describe "#grouped_by_date", :vcr  do
     
-  end
+  # end
 
-  describe "#count_by_date", :vcr  do
+  # describe "#count_by_date", :vcr  do
     
-  end
+  # end
 end
